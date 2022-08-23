a2fc8440aabbbd794f0cf37779e7847ef9a1a0eb
urodev <ourcssdev@gmail.com>
2022-08-23 12:05:13 -0500

    Lake of Kalandra Choosing Starter Build

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* import { sayHello } from "./js/components"; */
+import { chooseMyBuild } from "./js/buildComponent";
+import "./styles.css";
+/* const name = "Uriel";
+sayHello(name); */
+chooseMyBuild();
